0b25a5410a51b14959177364fb1e0c50f7157453
shah abir <abir_mtl@hotmail.com>
2020-02-06 11:13:14 -0500

    created database for user and admin, also template path configured

--- TNI_Pycharm/TNI_Project/timeline/urls.py
+++ TNI_Pycharm/TNI_Project/timeline/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='timeline-home'),
+
+
+    path('', views.index, name='timeline-base'),
     path('about/', views.about, name='timeline-about'),
+    path('registration/', views.register, name='user-registration'),
+    path('home/', views.home, name='timeline-home'),
+
 ]
