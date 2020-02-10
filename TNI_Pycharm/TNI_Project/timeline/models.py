2b336c00333493ddfebee66ce5e048f00717568b
shah abir <abir_mtl@hotmail.com>
2020-02-09 23:10:00 -0500

    User registration and uploading picture

--- TNI_Pycharm/TNI_Project/timeline/models.py
+++ TNI_Pycharm/TNI_Project/timeline/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class userProfile(models.Model):
+
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+    portfolio = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_pics')
+
+    def __str__(self):
+        return self.user.username
+
+class Image(models.Model):
+    name = models.CharField(max_length=50)
+    Img = models.ImageField(upload_to='images/')
--- TNI_Pycharm/TNI_Project/timeline/urls.py
+++ TNI_Pycharm/TNI_Project/timeline/urls.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-
-
     path('', views.index, name='timeline-base'),
     path('about/', views.about, name='timeline-about'),
     path('registration/', views.register, name='user-registration'),
+    path('registration/registeruser/', views.registerUser, name='user-registration'),
     path('home/', views.home, name='timeline-home'),
+    path('home/imageupload/', views.image_view, name='image_view'),
 
 ]
