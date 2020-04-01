45d602929621227f4a3f9ff39107a9f0e64bde48
AdrienKamran <adrien.kamran@gmail.com>
2020-04-01 15:11:58 -0400

    Moving tests from pytest to native Django testing

--- TNI_Pycharm/TNI_Project/timeline/tests.py
+++ TNI_Pycharm/TNI_Project/timeline/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+from TNI_Project.timeline import views
+from django.contrib.auth import authenticate
 # Create your tests here.
+
+
+def test_login_authentication():
+    user = authenticate(username='Shah', password='123')
+    if user is not None:
+        assert True
+    else:
+        assert False
