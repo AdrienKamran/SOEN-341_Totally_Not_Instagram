4a21b6b506819b62c2f6058901b7037044c54bee
AdrienKamran <adrien.kamran@gmail.com>
2020-04-01 15:30:51 -0400

    Update tests.py

--- TNI_Pycharm/TNI_Project/timeline/tests.py
+++ TNI_Pycharm/TNI_Project/timeline/tests.py
@@ -4,9 +4,10 @@ from django.contrib.auth import authenticate
 # Create your tests here.
 
 
-def test_login_authentication():
-    user = authenticate(username='Shah', password='123')
-    if user is not None:
-        assert True
-    else:
-        assert False
+class UnitTestCase(TestCase):
+    def test_login_authentication(self):
+        user = authenticate(username='Shah', password='123')
+        if user is not None:
+            assert True
+        else:
+            assert False
